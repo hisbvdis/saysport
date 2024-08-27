425aa8dedda020e145ba00c5da4d2bf04e445220
Roma Zvarych <hisbvdis@gmail.com>
2024-08-27 19:15:51 +0300

    photos env

--- app/(router)/api/photos/route.ts
+++ app/(router)/api/photos/route.ts
@@ -4,14 +4,14 @@ import { writeFile } from "node:fs/promises";
 import { existsSync, mkdirSync, unlinkSync } from "node:fs";
 
 export async function POST(request:NextRequest) {
-  if (!existsSync(`./public/${process.env.NEXT_PUBLIC_PHOTOS_PATH}`)) mkdirSync(`./public/${process.env.NEXT_PUBLIC_PHOTOS_PATH}`);
+  if (!existsSync(`${process.env.PHOTO_UPLOAD_PATH}`)) mkdirSync(`${process.env.PHOTO_UPLOAD_PATH}`);
   const formData = await request.formData();
   const files: File[] = formData.getAll("file") as File[];
   const names: string[] = formData.getAll("name") as string[];
   const photos = files.reduce<{file:File; name:string}[]>((acc, file, i) => acc.concat({file: file, name: names[i]}), []);
   for (const {file, name} of photos) {
     const buffer = Buffer.from(await file.arrayBuffer());
-    await writeFile(`./public/photos/${name}`, buffer);
+    await writeFile(`${process.env.PHOTO_UPLOAD_PATH}/${name}`, buffer);
     // const compressedFile = await sharp(buffer)
     //   .resize({ width: 2560, height: 2560, fit: "inside", withoutEnlargement: true })
     //   .webp({quality: 70})
@@ -25,7 +25,7 @@ export async function DELETE(request:NextRequest) {
   const formData = await request.formData();
   const names = formData.getAll("name");
   for (const name of names) {
-    if (existsSync(`./public/${process.env.NEXT_PUBLIC_PHOTOS_PATH}/${name}`)) unlinkSync(`./public/${process.env.NEXT_PUBLIC_PHOTOS_PATH}/${name}`);
+    if (existsSync(`${process.env.PHOTO_UPLOAD_PATH}/${name}`)) unlinkSync(`${process.env.PHOTO_UPLOAD_PATH}/${name}`);
   }
   return NextResponse.json("Ok");
 }
