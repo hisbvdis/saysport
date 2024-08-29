6c2095260f0b753efbc92a3f5ac353c077bd39b8
Roma Zvarych <hisbvdis@gmail.com>
2024-08-29 11:18:26 +0300

    knip

--- app/_components/ui/FieldSet/index.ts
+++ app/_components/ui/FieldSet/index.ts
@@ -1,6 +1,3 @@
 import FieldSet from "./FieldSet";
-import FieldSetLegend from "./FieldSetLegend";
-import { FieldSetContext } from "./FieldSet";
 
-export { FieldSet, FieldSetContext };
-export { FieldSetLegend };
+export { FieldSet };
